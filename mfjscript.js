d39d5e64e1783bc5ea60bc741d631ff5133d6702
David Perea <david.perea1@hyatt.com>
2017-08-19 12:44:39 -0500

    Completing template for new layout

---
+++ mfjscript.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const ANIMATION_SPEED = 300;
+
+let isAnimating = false,
+    isOpen = false;
+
+function show() {
+    $('[data-js="navlist"]').removeClass('d-none');
+    $('[data-js="navlist-options"]').animate({ left: '0'}, ANIMATION_SPEED, () => {
+        isAnimating = false;
+        isOpen = true;
+    });
+    $('[data-js="navlist-overlay"]').animate({ backgroundColor: 'rgba(0,0,0,0.6)'}, ANIMATION_SPEED);
+}
+
+function hide() {
+    $('[data-js="navlist-options"]').animate({ left: '-70%'}, ANIMATION_SPEED, () => {
+        isAnimating = false;
+        isOpen = false;
+        $('[data-js="navlist"]').addClass('d-none');
+    });
+    $('[data-js="navlist-overlay"]').animate({ backgroundColor: 'rgba(0,0,0,0)'}, ANIMATION_SPEED);
+}
+
+$(function() {
+    $('[data-js="navlist-button"]').on('click', function (e) {
+        // don't open/close if we're animating
+        if (!isAnimating) {
+            isAnimating = true;
+            show();
+        }
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    });
+
+    $('[data-js="navlist-overlay"]').on('click', function (e) {
+        if (e.target == this) {
+            if (!isAnimating) {
+                isAnimating = true;
+                hide();
+            }
+        }
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    });
+});
+
